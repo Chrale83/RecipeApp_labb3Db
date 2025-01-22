18225c9107e63431cfce6671ab40cd0b3190c07e
Christian Alexandersson <Chrale83@outlook.com>
2025-01-22 23:23:27 +0100

    -started with editing recipe

--- RecipeApp_labb3Db/ViewModels/EditRecipeViewModel.cs
+++ RecipeApp_labb3Db/ViewModels/EditRecipeViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RecipeDbAccess.Models;
+using System.Collections.ObjectModel;
 
 namespace RecipeApp_labb3Db.Presentation.ViewModels
 {
     class EditRecipeViewModel
     {
+        private ObservableCollection<Recipe>? _recipes;
+
+        public ObservableCollection<Recipe>? Recipes
+        {
+            get => _recipes;
+            set
+            {
+                _recipes = value;
+            }
+        }
+
+        private Recipe _selectedRecipe;
+
+        public Recipe SelectedRecipe
+        {
+            get { return _selectedRecipe; }
+            set { _selectedRecipe = value; }
+        }
+
+
     }
 }
